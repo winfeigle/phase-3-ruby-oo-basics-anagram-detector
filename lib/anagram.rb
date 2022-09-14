a45e619976131d5afaf8cecbbf4826b2a23b6631
Win Feigle <win.feigle@gmail.com>
2022-09-14 16:43:29 -0600

    commit tree fe3a35135e75d24793a096822941a4fb3435c52d

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :name
+
+    def initialize(word)
+        @name = word
+    end
+
+    def match(arr)
+        arr.select { |x| x.split("").sort == @name.split("").sort }
+    end
+end
